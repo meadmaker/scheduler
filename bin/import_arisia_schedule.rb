b44bfd5089121a3c54f43da7c710e42666d9e136
unknown <mark@.(none)>
2010-11-05 08:13:30 -0400

    Got the import working with the ISO-8859-1 character set.

--- bin/import_arisia_schedule.rb
+++ bin/import_arisia_schedule.rb
@@ -1,23 +1,26 @@
-puts "one"
+puts "one #{Time.now.to_s}"
 require File.expand_path('../../config/environment', __FILE__)
-puts "two"
+puts "two #{Time.now.to_s}"
 require 'csv'
-puts "three"
+puts "three #{Time.now.to_s}"
 
 event = Event.where("name = ?", ARGV[0]).first
 puts "Got event: #{event.name}"
 
+puts "four #{Time.now.to_s}"
 
 Presentation.delete_all
 puts "Deleted all presentations."
 
-# f = open(ARGV[1])
-# text = f.read().encode("UTF-8")
-# 
-# CSV.parse(text, {:headers => true}) do |row|
+puts "five #{Time.now.to_s}"
+
+f = open(ARGV[1], "r:ISO-8859-1")
+text = f.read()
+
+CSV.parse(text, {:headers => true}) do |row|
 
 
-CSV.foreach(ARGV[1], {:headers => true}) do |row|
+# CSV.foreach(ARGV[1], {:headers => true}) do |row|
   next if row['title'].nil?
   puts "Creating presentation: #{row['title']} (#{event.name})"
   p = Presentation.create(:name => row['title'],
